b0c6c87fc919a072cca0c7e86d0da14c787231bf
GaryCho <honor1998321@gmail.com>
2024-11-07 18:47:32 -0600

    Add controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,29 @@
 Rails.application.routes.draw do
+  get "orders/new"
+  get "orders/create"
+  get "orders/show"
+  get "cart/index"
+  get "categories/index"
+  get "categories/show"
+  get "products/index"
+  get "products/show"
+  get "home/index"
+   # Admin routes
+   namespace :admin do
+    root "dashboard#index" # Admin dashboard
+    resources :products
+    resources :orders, only: [ :index, :show, :edit, :update ]
+    resources :users, only: [ :index ]
+    resources :categories
+  end
+
+  # Frontend routes
+  root "home#index" # Main store page
+  resources :products, only: [ :index, :show ]
+  resources :categories, only: [ :index, :show ]
+  resources :cart, only: [ :index, :create, :update, :destroy ]
+  resources :orders, only: [ :new, :create, :show ]
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
