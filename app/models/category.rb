ac7f64cee9ad7a89805d8847497285aa591c85b6
GaryCho <honor1998321@gmail.com>
2024-11-07 18:03:30 -0600

    Add product_category model

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,4 @@
 class Category < ApplicationRecord
-  has_and_belongs_to_many :products
+  has_many :product_categories
+  has_many :products, through: :product_categories
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,6 @@
 class Product < ApplicationRecord
+  has_many :product_categories
+  has_many :categories, through: :product_categories
   has_many :order_items
-  has_and_belongs_to_many :categories
   has_many :product_pricing_histories
 end
