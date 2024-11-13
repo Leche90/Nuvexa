d3a5ed049445047b3af68ee92d595651c27adca2
GaryCho <honor1998321@gmail.com>
2024-11-12 18:58:33 -0600

    Add province model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_11_09_042142) do
+ActiveRecord::Schema[7.2].define(version: 2024_11_13_004109) do
   create_table "active_storage_attachments", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -117,6 +117,20 @@ ActiveRecord::Schema[7.2].define(version: 2024_11_09_042142) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "provinces", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "code", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "static_pages", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "username"
     t.datetime "created_at", null: false
@@ -127,7 +141,9 @@ ActiveRecord::Schema[7.2].define(version: 2024_11_09_042142) do
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.string "role", default: "customer"
+    t.bigint "province_id", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["province_id"], name: "index_users_on_province_id"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
