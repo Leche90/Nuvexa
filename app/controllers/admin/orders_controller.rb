a464ed45cf573e8c5c2fe0503e2470cfba54f234
GaryCho <honor1998321@gmail.com>
2024-11-14 22:27:42 -0600

    Add admin order page

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,6 +1,8 @@
 class Admin::OrdersController < ApplicationController
+  before_action :authenticate_admin_user!
   layout "admin"
   def index
+    @customers = User.includes(orders: [ :order_items, :address ]).all
   end
 
   def show
